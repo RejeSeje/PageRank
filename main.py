484816c347311dbd53f2645badd7f4d28eef6466
viggostarcke <vist@itu.dk>
2024-11-12 03:21:17 +0100

    ADD: random_surfer, tolerance feat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import random
+
 import matplotlib.pyplot as plt
 import networkx as nx
 import matplotlib
+from networkx import pagerank
+
 
 def construct_graph():
     with open('data/p2p-Gnutella08-mod.txt', 'rb') as fh:
@@ -40,15 +44,17 @@ def get_initial_ranking_vector(G):
 
     return ranking_vector
 
-def calculate_pagerank(G, damping_factor=0.85, max_iterations=100):
+def calculate_pagerank(G, damping_factor=0.85, max_iterations=100, tolerance=1e-10):
     # setup
     n = max(G.nodes()) + 1
     rv = get_initial_ranking_vector(G)  # x_0
     dangling_list = get_dangling_list(G)  # list of indices of dangling nodes (no outgoing edges)
     G_reversed = get_reverse(G)
 
+    previous_top_10 = None
+
     # outer loop
-    for _ in range(max_iterations):
+    for iter in range(max_iterations):
         x_next = [0] * n  # x_{k+1}
 
         dangling_sum = sum(rv[node] for node in dangling_list)
@@ -61,17 +67,57 @@ def calculate_pagerank(G, damping_factor=0.85, max_iterations=100):
             for j in G_reversed[i]:  # Ax_k
                 x_next[i] += damping_factor * rv[j] / G.out_degree(j)
 
+        current_top_10 = sorted(enumerate(x_next), key=lambda x: x[1], reverse=True)[:10]
+
+        # check for stability by the tolerance standard
+        if previous_top_10:
+            changes = [abs(current_top_10[i][1] - previous_top_10[i][1]) for i in range(10)]
+            if all(change < tolerance for change in changes):
+                # print(f"top 10 nodes stabilised after {iter} iterations.")
+                return x_next, iter
+
+        previous_top_10 = current_top_10
         rv = x_next
 
-    return rv
+    print("max iterations reached without full stabilisation of top 10 nodes.")
+    return rv, iter
 
+def random_surfer(G, damping_factor=0.85, jumps=100000):
+    n = max(G.nodes()) + 1
+    visits = [0] * n  # list of visits for every site
+
+    curr_node = random.choice(list(G.nodes()))
+
+    for _ in range(jumps):
+        visits[curr_node] += 1
+
+        if random.random() < damping_factor and list(G.successors(curr_node)):
+            curr_node = random.choice(list(G.successors(curr_node)))
+        else:
+            curr_node = random.choice(list(G.nodes()))
+
+    rv = [visit_count / jumps for visit_count in visits]
+    return rv
 
 if __name__ == '__main__':
     G = construct_graph()
-    pagerank_list = calculate_pagerank(G)
+    random_surfer_list = random_surfer(G)
+    pagerank_list_5th_digit, iter_5th = calculate_pagerank(G, tolerance=1e-5)
+    pagerank_list_10th_digit, iter_10th = calculate_pagerank(G, tolerance=1e-10)
 
-    sorted_pagerank = sorted(enumerate(pagerank_list), key=lambda x: x[1], reverse=True)
-    print("the top 10 nodes:")
-    for node, rank in sorted_pagerank[:10]:
-        print(f"Node {node}: PageRank {rank}")
-        #print(f"Node {node}: PageRank {rank:.4f}")  # limit floating digits
+    sorted_random_surfer = sorted(enumerate(random_surfer_list), key=lambda x: x[1], reverse=True)
+    sorted_pagerank_list_5th_digit = sorted(enumerate(pagerank_list_5th_digit), key=lambda x: x[1], reverse=True)
+    sorted_pagerank_list_10th_digit = sorted(enumerate(pagerank_list_10th_digit), key=lambda x: x[1], reverse=True)
+    print(f"the top 10 nodes for random surfing")
+    for node, rank in sorted_random_surfer[:10]:
+        print(f"Node {node}: PageRank {rank:.5f}")
+    print("\n")
+
+    print(f"the top 10 nodes for tolerance on 5th digit\nreached after {iter_5th} iterations:")
+    for node, rank in sorted_pagerank_list_5th_digit[:10]:
+        print(f"Node {node}: PageRank {rank:.5f}")
+    print("\n")
+
+    print(f"the top 10 nodes for tolerance on 10th digit\nreached after {iter_10th} iterations:")
+    for node, rank in sorted_pagerank_list_10th_digit[:10]:
+        print(f"Node {node}: PageRank {rank:.10f}")
